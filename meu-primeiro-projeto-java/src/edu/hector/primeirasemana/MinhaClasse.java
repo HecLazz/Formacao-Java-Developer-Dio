e6c57c6a3517d10a6156392e88631c012e4ac03e
HecLazz <hectorlazzari20@gmail.com>
2024-01-24 19:59:03 -0300

    atualizações

---
+++ meu-primeiro-projeto-java/src/edu/hector/primeirasemana/MinhaClasse.java
@@ -0,0 +1,7 @@
+package edu.hector.primeirasemana;
+
+public class MinhaClasse { //Testes para execução via terminal
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo terminal");
+    }
+}
